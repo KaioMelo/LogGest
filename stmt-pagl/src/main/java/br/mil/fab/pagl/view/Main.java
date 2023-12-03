aa51af8c1c18bb771779c9b72abbd4f3be2ec0d4
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2023-12-03 12:33:44 -0300

    Adicionando dependência JavaFX e configurando persistence.xml

--- stmt-pagl/src/main/java/br/mil/fab/pagl/view/Main.java
+++ stmt-pagl/src/main/java/br/mil/fab/pagl/view/Main.java
@@ -1,34 +1,20 @@
 package br.mil.fab.pagl.view;
 
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.application.Application;
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) {
-        // Criar componentes da interface do usuário
-        Button btn = new Button("Clique em mim!");
-
-        // Configurar a lógica de evento
-        btn.setOnAction(e -> System.out.println("Olá, JavaFX!"));
-
-        // Configurar o layout da interface do usuário
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        // Configurar a cena
-        Scene scene = new Scene(root, 300, 250);
-
-        // Configurar o palco
-        primaryStage.setTitle("JavaFX sem FXML");
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("JavaFX Maven Project");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
     }
