7e3f994016635233d5956689715251f3c13a00e0
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2023-12-11 21:51:00 -0300

    Alterando nome da função

--- stmt-pagl/src/main/java/br/mil/fab/pagl/controller/FXMLHomeController.java
+++ stmt-pagl/src/main/java/br/mil/fab/pagl/controller/FXMLHomeController.java
@@ -1,22 +1,22 @@
 package br.mil.fab.pagl.controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
-import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FXMLHomeController implements Initializable {
-
     @FXML
     private MenuItem menuItemVeiculo;
+    @FXML
+    private MenuItem menuItemMotorista;
+    @FXML
+    private AnchorPane anchorPane;
 
     @Override
     public void initialize(URL url, ResourceBundle rB) {
@@ -24,7 +24,8 @@ public class FXMLHomeController implements Initializable {
     }
 
     @FXML
-    public void entrarEmInterfaceVeiculo () throws IOException{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/FXMLVeiculo.fxml"));
+    public void handleMenuItemVeiculo () throws IOException{
+        AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("/view/FXMLVeiculo.fxml"));
+        anchorPane.getChildren().setAll(a);
     }
 }
